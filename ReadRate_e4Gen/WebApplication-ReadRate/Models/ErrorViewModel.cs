37c908c11d3eebed1bdd7796be070b700564ea33
Marina <marinaabadmor@gmail.com>
2025-12-15 20:43:54 +0100

    Limpieza de archivos (borrado)

--- ReadRate_e4Gen/WebApplication-ReadRate/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication_ReadRate.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

15bcf0f713e8a691979d7158cc2d36b6fc26b4cc
Marina <marinaabadmor@gmail.com>
2025-12-15 17:05:29 +0100

    Boostrap / Media Queries / Responsive / Barra de Navegacion / Etc - APLICADO A ELEMENTOS DEL SCAFFOLDING

--- ReadRate_e4Gen/WebApplication-ReadRate/obj/Debug/net8.0/WebApplication-ReadRate.AssemblyInfo.cs
+++ ReadRate_e4Gen/WebApplication-ReadRate/obj/Debug/net8.0/WebApplication-ReadRate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication-ReadRate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f502928084310499ad707b246dbd32de32d1b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbdfd80e10daa9e2791b5ee2e6aa1bd870a48ebb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication-ReadRate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication-ReadRate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
